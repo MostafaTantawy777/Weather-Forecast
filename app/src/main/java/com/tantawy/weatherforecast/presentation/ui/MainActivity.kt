01d708e3cd055ba2fe55d492e6a6e6e42b3810a7
Mostafa Tantawy <m.tantawy@sary.com>
2023-11-25 18:50:23 +0200

    set data with data binding

--- app/src/main/java/com/tantawy/weatherforecast/presentation/ui/MainActivity.kt
+++ app/src/main/java/com/tantawy/weatherforecast/presentation/ui/MainActivity.kt
@@ -1,30 +1,21 @@
 package com.tantawy.weatherforecast.presentation.ui
 
 import android.os.Bundle
-import android.util.Log
-import androidx.lifecycle.lifecycleScope
-import com.tantawy.weatherforecast.R
 import com.tantawy.weatherforecast.base.activity.BaseActivity
+import com.tantawy.weatherforecast.databinding.ActivityMainBinding
 import com.tantawy.weatherforecast.presentation.viewmodel.WeatherViewModel
-import kotlinx.coroutines.launch
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MainActivity : BaseActivity() {
 
-    private val productsViewModel: WeatherViewModel by viewModel()
+    private lateinit var binding: ActivityMainBinding
+    private val weatherViewModel: WeatherViewModel by viewModel()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        lifecycleScope.launch {
-            productsViewModel.apply {
-                weather.observe(this@MainActivity) {
-                    it?.let {
-                        Log.e("MainActivity", it[0]?.main?.humidity.toString())
-                    }
-                }
-            }
-        }
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding?.lifecycleOwner = this
+        binding?.weather = weatherViewModel
     }
 }
