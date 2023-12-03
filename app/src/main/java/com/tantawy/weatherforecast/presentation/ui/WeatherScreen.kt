d0d87ae6d051e69008e30252b133ae2fa0d29d62
Mostafa Tantawy <m.tantawy@sary.com>
2023-12-03 04:46:04 +0200

    # add progress bar

--- app/src/main/java/com/tantawy/weatherforecast/presentation/ui/WeatherScreen.kt
+++ app/src/main/java/com/tantawy/weatherforecast/presentation/ui/WeatherScreen.kt
@@ -1,12 +1,15 @@
 import android.annotation.SuppressLint
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -15,6 +18,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
@@ -232,6 +236,18 @@ fun weatherScreen(modifier: Modifier, viewModel: WeatherViewModel, city: String)
         }
 
     } else {
+        CircularIndeterminateProgressBar()
         Text("No data available")
+    }
+}
+
+@Composable
+fun CircularIndeterminateProgressBar() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        CircularProgressIndicator()
     }
 }
